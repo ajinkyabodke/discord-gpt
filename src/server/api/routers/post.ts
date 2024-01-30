a34345dc163a0c5194f248d2634a7ce9f6f53fb1
Ajinkya Bodke <ajinkyabodke678@gmail.com>
2024-01-30 20:19:00 +0530

    initial commit

---
+++ src/server/api/routers/post.ts
@@ -0,0 +1,14 @@
+import { z } from "zod";
+
+import { createTRPCRouter, publicProcedure } from "@/server/api/trpc";
+import { posts } from "@/server/db/schema";
+
+export const postRouter = createTRPCRouter({
+  hello: publicProcedure
+    .input(z.object({ text: z.string() }))
+    .query(({ input }) => {
+      return {
+        greeting: `Hello ${input.text}`,
+      };
+    }),
+});
