a3207b171285ae1de60d236f9bba502af72bbfc4
Ajinkya Bodke <ajinkyabodke678@gmail.com>
2024-01-31 17:07:12 +0530

    add chat input

--- src/app/(main)/servers/[serverId]/page.tsx
+++ src/app/(main)/servers/[serverId]/page.tsx
@@ -1,11 +1,14 @@
 import { UserButton } from "@clerk/nextjs";
 import { ModeToggle } from "@/components/mode-toggle";
 import { ChatHeader } from "@/components/chat/chat-header";
+import { ChatInput } from "@/components/chat/chat-input";
 
 export default async function Home() {
   return (
     <div className="flex h-full w-screen flex-col bg-white dark:bg-[#313338]">
-      <ChatHeader />    
+      <ChatHeader />
+      <div className="flex-1">Messages</div>
+      <ChatInput />
     </div>
   );
 }
---
+++ src/components/chat/chat-input.tsx
@@ -0,0 +1,79 @@
+"use client";
+
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "../ui/input";
+import { Plus } from "lucide-react";
+import { EmojiPicker } from "../emoji-picker";
+
+const formSchema = z.object({
+  message: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+  }),
+});
+
+export const ChatInput = () => {
+  // 1. Define your form.
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      message: "",
+    },
+  });
+
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values);
+    form.reset();
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)}>
+        <FormField
+          control={form.control}
+          name="message"
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <div className="relative p-4 pb-6">
+                  <button
+                    type="button"
+                    className="absolute left-8 top-7 flex h-[24px] w-[24px] items-center justify-center rounded-full bg-zinc-500 p-1 transition hover:bg-zinc-600 dark:bg-zinc-400 dark:hover:bg-zinc-300"
+                  >
+                    <Plus className="text-white dark:text-[#313338]" />
+                  </button>
+                  <Input
+                    className="border-0 border-none bg-zinc-200/90 px-14 py-6 text-zinc-600 focus-visible:ring-0 focus-visible:ring-offset-0 dark:bg-zinc-700/75 dark:text-zinc-200"
+                    placeholder={`Message`}
+                    {...field}
+                  />
+                  <div className="absolute right-8 top-7">
+                    <EmojiPicker
+                      onChange={(emoji: string) =>
+                        field.onChange(`${field.value} ${emoji}`)
+                      }
+                    />
+                  </div>
+                </div>
+              </FormControl>
+            </FormItem>
+          )}
+        />
+      </form>
+    </Form>
+  );
+};
