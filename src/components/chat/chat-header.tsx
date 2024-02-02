c18e1ac0509766ed0e28be6346d268d7b71f14a5
Ajinkya Bodke <ajinkyabodke678@gmail.com>
2024-02-02 12:39:15 +0530

    threads

--- src/components/chat/chat-header.tsx
+++ src/components/chat/chat-header.tsx
@@ -65,25 +65,90 @@ export const ChatHeader = () => {
         </div>
       </div>
       <div className="flex items-center gap-x-1">
-        <button className="mb-[2px] mr-2 flex h-6 w-6 items-center justify-center ">
-          <svg
-            x={0}
-            y={0}
-            className="icon__4cb88"
-            aria-hidden="true"
-            role="img"
-            xmlns="http://www.w3.org/2000/svg"
-            width={24}
-            height={24}
-            fill="none"
-            viewBox="0 0 24 24"
-          >
-            <path
-              d="M12 2.81a1 1 0 0 1 0-1.41l.36-.36a1 1 0 0 1 1.41 0l9.2 9.2a1 1 0 0 1 0 1.4l-.7.7a1 1 0 0 1-1.3.13l-9.54-6.72a1 1 0 0 1-.08-1.58l1-1L12 2.8ZM12 21.2a1 1 0 0 1 0 1.41l-.35.35a1 1 0 0 1-1.41 0l-9.2-9.19a1 1 0 0 1 0-1.41l.7-.7a1 1 0 0 1 1.3-.12l9.54 6.72a1 1 0 0 1 .07 1.58l-1 1 .35.36ZM15.66 16.8a1 1 0 0 1-1.38.28l-8.49-5.66A1 1 0 1 1 6.9 9.76l8.49 5.65a1 1 0 0 1 .27 1.39ZM17.1 14.25a1 1 0 1 0 1.11-1.66L9.73 6.93a1 1 0 0 0-1.11 1.66l8.49 5.66Z"
-              className="fill-zince-500  hover:fill-zinc-700 dark:fill-zinc-400 dark:hover:fill-zinc-100"
-            />
-          </svg>
-        </button>
+        <DropdownMenu>
+          <DropdownMenuTrigger className="focus:outline-none" asChild>
+            <button className="mb-[2px] mr-2 flex h-6 w-6 items-center justify-center ">
+              <svg
+                x={0}
+                y={0}
+                className="icon__4cb88"
+                aria-hidden="true"
+                role="img"
+                xmlns="http://www.w3.org/2000/svg"
+                width={24}
+                height={24}
+                fill="none"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  d="M12 2.81a1 1 0 0 1 0-1.41l.36-.36a1 1 0 0 1 1.41 0l9.2 9.2a1 1 0 0 1 0 1.4l-.7.7a1 1 0 0 1-1.3.13l-9.54-6.72a1 1 0 0 1-.08-1.58l1-1L12 2.8ZM12 21.2a1 1 0 0 1 0 1.41l-.35.35a1 1 0 0 1-1.41 0l-9.2-9.19a1 1 0 0 1 0-1.41l.7-.7a1 1 0 0 1 1.3-.12l9.54 6.72a1 1 0 0 1 .07 1.58l-1 1 .35.36ZM15.66 16.8a1 1 0 0 1-1.38.28l-8.49-5.66A1 1 0 1 1 6.9 9.76l8.49 5.65a1 1 0 0 1 .27 1.39ZM17.1 14.25a1 1 0 1 0 1.11-1.66L9.73 6.93a1 1 0 0 0-1.11 1.66l8.49 5.66Z"
+                  className="fill-zince-500  hover:fill-zinc-700 dark:fill-zinc-400 dark:hover:fill-zinc-100"
+                />
+              </svg>
+            </button>
+          </DropdownMenuTrigger>
+
+          <DropdownMenuContent className="w-[27rem] space-y-[2px] text-sm font-medium text-black dark:bg-[#111214] dark:bg-[#1e1f22] dark:text-[#b5bac1]">
+            <DropdownMenuItem className="flex cursor-pointer items-center justify-between px-3 py-2 text-sm hover:bg-violet-500 hover:text-white dark:bg-[#1e1f22]">
+              <div className="flex items-center justify-center gap-x-2">
+                <svg
+                  x={0}
+                  y={0}
+                  className="icon__4cb88"
+                  aria-hidden="true"
+                  role="img"
+                  xmlns="http://www.w3.org/2000/svg"
+                  width={24}
+                  height={24}
+                  fill="none"
+                  viewBox="0 0 24 24"
+                >
+                  <path
+                    d="M12 2.81a1 1 0 0 1 0-1.41l.36-.36a1 1 0 0 1 1.41 0l9.2 9.2a1 1 0 0 1 0 1.4l-.7.7a1 1 0 0 1-1.3.13l-9.54-6.72a1 1 0 0 1-.08-1.58l1-1L12 2.8ZM12 21.2a1 1 0 0 1 0 1.41l-.35.35a1 1 0 0 1-1.41 0l-9.2-9.19a1 1 0 0 1 0-1.41l.7-.7a1 1 0 0 1 1.3-.12l9.54 6.72a1 1 0 0 1 .07 1.58l-1 1 .35.36ZM15.66 16.8a1 1 0 0 1-1.38.28l-8.49-5.66A1 1 0 1 1 6.9 9.76l8.49 5.65a1 1 0 0 1 .27 1.39ZM17.1 14.25a1 1 0 1 0 1.11-1.66L9.73 6.93a1 1 0 0 0-1.11 1.66l8.49 5.66Z"
+                    className="fill-zince-500  hover:fill-zinc-700 dark:fill-zinc-400 dark:hover:fill-zinc-100"
+                  />
+                </svg>
+                <span>Threads</span>
+              </div>
+
+              <div className="hidden h-[30px] w-[180px] md:block">
+                <Command>
+                  <CommandInput placeholder="Search for threads" />
+                </Command>
+              </div>
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem className=" flex h-[20rem] cursor-pointer items-center justify-between px-3 py-2 text-sm text-white dark:bg-[#2b2d31] ">
+              <div className="w-full flex-col items-center justify-center gap-2 align-middle">
+                <div className="flex justify-center">
+                  <svg
+                    aria-hidden="true"
+                    role="img"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width={36}
+                    height={36}
+                    fill="none"
+                    viewBox="0 0 24 24"
+                  >
+                    <path
+                      d="M12 2.81a1 1 0 0 1 0-1.41l.36-.36a1 1 0 0 1 1.41 0l9.2 9.2a1 1 0 0 1 0 1.4l-.7.7a1 1 0 0 1-1.3.13l-9.54-6.72a1 1 0 0 1-.08-1.58l1-1L12 2.8ZM12 21.2a1 1 0 0 1 0 1.41l-.35.35a1 1 0 0 1-1.41 0l-9.2-9.19a1 1 0 0 1 0-1.41l.7-.7a1 1 0 0 1 1.3-.12l9.54 6.72a1 1 0 0 1 .07 1.58l-1 1 .35.36ZM15.66 16.8a1 1 0 0 1-1.38.28l-8.49-5.66A1 1 0 1 1 6.9 9.76l8.49 5.65a1 1 0 0 1 .27 1.39ZM17.1 14.25a1 1 0 1 0 1.11-1.66L9.73 6.93a1 1 0 0 0-1.11 1.66l8.49 5.66Z"
+                      fill="currentColor"
+                      className
+                    />
+                  </svg>
+                </div>
+                <span className="flex justify-center py-2 text-2xl font-semibold dark:text-white">
+                  There are no threads.
+                </span>
+                <span className="flex justify-center py-2 text-center text-slate-400 dark:text-white">
+                  Stay focused on a conversation with a thread, a temporary text
+                  channel.
+                </span>
+              </div>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+
         <DropdownMenu>
           <DropdownMenuTrigger className="focus:outline-none" asChild>
             <button>
