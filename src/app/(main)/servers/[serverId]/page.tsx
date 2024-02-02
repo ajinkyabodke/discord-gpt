2b83c7c3eb8f6d83dd961cd30c34194ec5d79cc5
Ajinkya Bodke <ajinkyabodke678@gmail.com>
2024-02-02 16:58:24 +0530

    adjust timezone for vercel

--- src/app/(main)/servers/[serverId]/page.tsx
+++ src/app/(main)/servers/[serverId]/page.tsx
@@ -19,21 +19,25 @@ export default async function Home() {
     .leftJoin(users, eq(messages.userId, users.userId))
     .orderBy(messages.createdAt);
 
-  const updatedArray = result.map((item) => {
-    item.messages.createdAt = new Date(item.messages.createdAt).toLocaleString(
-      "en-IN",
-      {
+    const updatedArray = result.map((item) => {
+      const createdAtDate = new Date(item.messages.createdAt);
+      
+      // Add 5 hours and 30 minutes
+      createdAtDate.setHours(createdAtDate.getHours() + 5);
+      createdAtDate.setMinutes(createdAtDate.getMinutes() + 30);
+    
+      // Format the date as per the specified options
+      item.messages.createdAt = createdAtDate.toLocaleString("en-IN", {
         hour12: true,
         month: "2-digit",
         day: "2-digit",
         year: "numeric",
         hour: "numeric",
         minute: "numeric",
-      },
-    );
-    return item;
-  });
-
+      });
+    
+      return item;
+    });
   return (
     <div className="flex h-full w-screen flex-col bg-white dark:bg-[#313338]">
       <ChatHeader />
